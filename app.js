0214ac3afcb24b268ab8bebaf08e3ea0795c06df
Addy Rodriguez <rdz.33.a@gmail.com>
2022-01-19 22:08:07 -0600

    Create Route Post

--- app.js
+++ app.js
@@ -2,52 +2,51 @@
 const express = require('express')
 const app = express()
 
-// const methodOverride =require('method-override')
-
-// const mongoose = require('mongoose')
 
 // configure app
-const PORT = 8000
+const PORT = 3000
 
 app.set('view engine', 'ejs')
 
 // middleware
-// app.use(methodOverride('_method'))
-// app.use(express.urlencoded({extended: false}))
+
 // body-parser middleware - creates a req.body{}
 app.use((req,res,next)=>{
-    console.log('after body parser')
     console.log(req.body)
     next()
 })
-
+app.get('/',(req,res)=>{
+    console.log('hitting home route')
+    res.send('home route')
+})
 // routing - logs
 
 //  new route
-app.get('/logs/new',(req,res) =>res.render('new.ejs'))
+app.get('/logs/new',(req,res) =>{
+    console.log('new route on')
+    res.render('new.ejs')
+ })
 
 // index route
-app.get('/logs/', (req,res)=> res.send('logs index'))
+app.get('/logs/', (req,res)=> {
+  res.send('logs index')
+})
 
 // create route POST
 app.post('/logs/', (req,res) => {
     console.log(req.body)
     if(req.body.shipIsBroken === "on"){
-        req.body.shipIsBroken = true
-    }else {
-        req.body.shipIsBroken = false
-    }
-
-    logs.push(req.body)
+       req.body.shipIsBroken = true
+   }else {
+       req.body.shipIsBroken = false
+   }
 
     res.send(req.body)
+   // logs.push(req.body)
+   // res.redirect('/logs/')
 })
 
 
-app.get('/',(req,res)=>{
-    console.log('hitting home route')
-    res.send('home route')
-})
 
 // Server
 app.listen(PORT, ()=>console.log('Listening on port:', PORT))
